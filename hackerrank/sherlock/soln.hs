1dd74c469d0293949cf7b414c6d5e36465b880d7
Ben Ritchie <benjamin.ritchie@gmail.com>
2015-10-27 08:29:14 +0000

    last soln worked but was too slow for large numbers

--- hackerrank/sherlock/soln.hs
+++ hackerrank/sherlock/soln.hs
@@ -2,10 +2,12 @@
 
 -- we know it has to be a multiple of 3 x '5' and 5 x '3' in the numbers
 -- we also know that the fives have to be first for maximum numbers but let's not rely on that.. 
-generateCandidates n = [ replicate (threes*5) '3' ++ replicate (fives*3) '5' | threes <- [0..n], fives <- [0..n] ]
-generateNumbers n = map (read::String->Integer) $ filter ((n==) . length) (generateCandidates n)
-generateMaxNumber n = if length (generateNumbers n) == 0 then "-1" else show $ maximum $ generateNumbers n
-generateMaxNumberStr str = generateMaxNumber (read str :: Int)
+-- generateCandidates n = [ replicate (fives*3) '5' ++ replicate (threes*5) '3' | threes <- [0..n], fives <- [0..n] ]
+let generateCandidates n = [ replicate (fives*3) '5' ++ replicate (n-fives*3) '3' | fives <- [0..n], fives `mod` 3 + (n - fives) `mod` 5 == 0 ]
+-- generateNumbers n = map (read::String->Integer) $ filter ((n==) . length) (generateCandidates n)
+let generateNumbers n = map (read::String->Integer) (generateCandidates n)
+let generateMaxNumber n = if generateNumbers n == [] then "-1" else show $ maximum $ generateNumbers n
+let generateMaxNumberStr str = generateMaxNumber (read str :: Int)
 
 -- pattern match first line as testCount and test cases as rest of lines
 processInput (testCount:testCases) =
